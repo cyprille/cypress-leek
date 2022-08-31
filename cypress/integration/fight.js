c98ff2915e0305e16a9bd910cefd4bc09fd8d195
Cyprille CHAUVRY <cyprille.chauvry@gmail.com>
2022-08-31 22:00:20 +0200

    Fixed bugs and improved performance with real fight time handling

--- cypress/integration/fight.js
+++ cypress/integration/fight.js
@@ -4,77 +4,92 @@
  * @license     Proprietary
  */
 
-describe('Fight workflow', () => {
-  before(() => {
-      cy.login()
-      cy.init()
+describe('Fight workflow', function () {
+  Cypress.Cookies.defaults({
+    preserve: 'token'
   })
 
-  it('Inits results', () => {
+  before(() => {
+    cy.clearCookies()
+
+    cy.loginCheck().then((logged) => {
+      if (logged === false) {
+        cy.log('Not logged in')
+        cy.login()
+      } else {
+        cy.log('Already logged')
+      }
+    })
+  })
+
+  it('Inits results', function () {
     cy.wrap(0).as('win')
     cy.wrap(0).as('lost')
   })
 
-  it('Launches the fight...', () => {
+  it('Launches the fight...', function () {
     cy.fight()
   })
+})
+
+Cypress.Commands.add('waitForFightToGenerate', (time) => {
+  cy.wait(time)
+
+  // Wait for the fight to really start
+  it('Quit the simulation mode', () => {
+    if (cy.get('.fight-actions').find('div').length === 0) {
+      cy.waitForFightToGenerate(1000)
+    }
+  })
+
+  // @TODO: Loop retry for the fight to really finish
+  it('Clicks on the retry button to access the fight\'s results if it\'s not finished yet', () => {
+    cy.get('body').then($body => {
+      if ($body.find('.v-icon.notranslate.mdi.mdi-refresh.theme--light').length > 0) {   
+        cy.get('.v-icon.notranslate.mdi.mdi-refresh.theme--light').click({ multiple: true, force: true })
+      }
+    });
+  })
+
+  it('Retrieves the winner leek', () => {
+    let winner = Cypress.$('.report-general > div > div > div > div.scroll-x > table > tr > td > a').attr('href')
+  })
+
+  it('Displays the winner =)', () => {
+    console.log(winner)
+  })
+
+  it('Logs the fight\'s results', () => {
+    (winner == ('/leek/' + Cypress.env('leek_id'))) ? cy.wrap(this.win + 1).as('win') : cy.wrap(this.lost + 1).as('lost')
+  })
+
+  it('Launches a new fight', () => {
+    cy.fight()
+  })
+
+  cy.fight()
 })
 
 /*
  * Fight random leek 
  * Only if there are sufficient remaining fights for the day...
  */
-Cypress.Commands.add('fight', (email, pw) => { 
-  it('Go to the garden', () => {
-    cy.visit('https://leekwars.com/garden/solo/81711')
-  })
+Cypress.Commands.add('fight', () => {
+  cy.visit("https://leekwars.com/garden/solo/" + Cypress.env('garden_id'))
+  cy.closeWhatsNewPopup()
   
-  it('Checks if there are sufficient remaining fights for the day', () => {
-    cy.get('.page-header.page-bar > div.tabs > div > span').then(function($number) {
-      if ($number.text() < 1) {
-        // @TODO: Send an email with the daily results
-        it('No more fights for today, stopping...', () => {
-          return
-        })
-      } else {
-        it('Click on a random opponent from 1 to 5', () => {
-          cy.get('.opponents .leek').eq((Math.floor(Math.random() * 10)) + 1).click()
-        })
+  // Checks if there are sufficient remaining fights for the day
+  cy.get('.page-header.page-bar > div.tabs > div > span').then(function($number) {
+    if ($number.text() < 1) {
+      // @TODO: Send an email with the daily results
+      it('No more fights for today, stopping...', () => {
+        return
+      })
+    } else {
+      // Click on a random opponent from 1 to 5
+      cy.get('.opponents .leek').eq(Math.floor(Math.random() * (5 - 1 + 1) + 1)).click()
 
-        it('Wait for the fight to launch', () => {
-          cy.wait(20000);
-        })
-
-        // @TODO: Wait for the fight to really start      
-        it('Quit the simulation mode', () => {
-          cy.get('body').trigger('keyup', { keyCode: 81});
-        })
-
-        // @TODO: Loop retry for the fight to really finish
-        it('Clicks on the retry button to access the fight\'s results if it\'s not finished yet', () => {
-          cy.get('body').then($body => {
-            if ($body.find('.v-icon.notranslate.mdi.mdi-refresh.theme--light').length > 0) {   
-              cy.get('.v-icon.notranslate.mdi.mdi-refresh.theme--light').click({ multiple: true, force: true })
-            }
-          });
-        })
-
-        it('Retrieves the winner leek', () => {
-          let winner = Cypress.$('.report-general > div > div > div > div.scroll-x > table > tr > td > a').attr('href')
-        })
-
-        it('Displays the winner =)', () => {
-          console.log(winner)
-        })
-
-        it('Logs the fight\'s results', () => {
-          (winner == '/leek/81711') ? cy.wrap(this.win + 1).as('win') : cy.wrap(this.lost + 1).as('lost')
-        })
-
-        it('Launches a new fight', () => {
-          cy.fight()
-        })
-      }
-    })
+      cy.waitForFightToGenerate(1000)
+    }
   })
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,30 +1,46 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-Cypress.Commands.add('login', () => {
+Cypress.Commands.add('loginCheck', () => {
   cy.visit('https://leekwars.com/login')
+  cy.closeWhatsNewPopup()
+
+  cy.get('.header-right').then(($e) => {
+    return $e.find('.header-farmer').length > 0
+  })
+})
+
+Cypress.Commands.add('login', () => {
+  cy.log('Go to the login page')
+
+  cy.visit('https://leekwars.com/login')
+  cy.closeWhatsNewPopup()
   
   cy.get('input[name="login"]')
-  .type('<EMAIL>')
-  .should('have.value', '<EMAIL>')
+  .type(Cypress.env('login'))
+  .should('have.value', Cypress.env('login'))
   
   cy.get('input[name="password"]')
-  .type('<PASSWORD>')
-  .should('have.value', '<PASSWORD>')
+  .type(Cypress.env('password'))
+  .should('have.value', Cypress.env('password'))
   
   cy.get('.v-input--selection-controls__ripple')
   .click({ multiple: true, force: true })
   
   cy.get('button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--large.primary')
   .click({ multiple: true, force: true })
+
+  cy.wait(1000)
+
+  cy.closeWhatsNewPopup()
 })
 
-Cypress.Commands.add('init', () => {
-  cy.visit('https://leekwars.com/garden/solo/81711')
+Cypress.Commands.add('closeWhatsNewPopup', () => {
+  cy.wait(1000)
 
-  cy.get('.actions>div>div')
-    .click({ multiple: true, force: true })
-
-  cy.get('.v-icon.notranslate.close.v-icon--link.mdi.mdi-close.theme--light')
-    .click({ multiple: true, force: true })
+  cy.get('body').then((body) => {
+    if (body.find('.v-dialog--active').length > 0) {
+      cy.get('.v-dialog--active > .actions > div').click();
+    }
+  })
 })
